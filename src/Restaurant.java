83f16534958c69dbb184d40d282e1ed478b354ff
ElmaRuis <elmatampubolon@gmail.com>
2023-04-17 14:24:10 +0800

    Tampilan Menu Restaurant

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -12,3 +12,35 @@ public class Restaurant{
         this.makanan = new ArrayList<>();
         this.minuman = new ArrayList<>();
     }
+
+    public Restaurant(int id, String nama, String alamat){
+        this.id = id;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.makanan = new ArrayList<>();
+        this.minuman = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
