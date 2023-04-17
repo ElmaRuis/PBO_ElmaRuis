209e3fd01a18b9d51e6e486da0d75bf0d02aa797
ElmaRuis <elmatampubolon@gmail.com>
2023-04-17 10:40:38 +0800

    Tampilan Order Restaurant

--- src/Order.java
+++ src/Order.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class Order {
     private int idRestaurant;
     private ArrayList<Menu> menuMakanan;
@@ -16,4 +17,60 @@ public class Order {
         qtyMenuMinuman = new ArrayList<>();
     }
 
+    public int getIdRestaurant() {
+        return idRestaurant;
+    }
+
+    public void setIdRestaurant(int idRestaurant) {
+        this.idRestaurant = idRestaurant;
+    }
+
+    public ArrayList<Menu> getMenuMakanan(){
+        return menuMakanan;
+    }
+
+    public void addMenuMakanan(Menu makanan){
+        this.menuMakanan.add(makanan);
+    }
+
+    public ArrayList<Integer> getQtyMenuMakanan() {
+        return qtyMenuMakanan;
+    }
+
+    public void addQtyMenuMakanan(int qty) {
+        this.qtyMenuMakanan.add(qty);
+    }
+
+    public ArrayList<Menu> getMenuMinuman(){
+        return menuMinuman;
+    }
+
+    public void addMenuMinuman(Menu minuman){
+        this.menuMinuman.add(minuman);
+    }
+
+    public ArrayList<Integer> getQtyMenuMinuman() {
+        return qtyMenuMinuman;
+    }
+
+    public void addQtyMenuMinuman(int qty) {
+        this.qtyMenuMinuman.add(qty);
+    }
+
+    public float getJarak() {
+        return jarak;
+    }
+
+    public void setJarak(float jarak) {
+        this.jarak = jarak;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+}
 
