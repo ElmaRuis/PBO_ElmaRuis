b916c7a89aa2c716ce5253db1f97269505230079
ElmaRuis <elmatampubolon@gmail.com>
2023-04-17 09:17:30 +0800

    Tampilan Menu Restaurant

---
+++ src/Order.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Order {
+    private int idRestaurant;
+    private ArrayList<Menu> menuMakanan;
+    private ArrayList<Integer> qtyMenuMakanan;
+    private ArrayList<Menu> menuMinuman;
+    private ArrayList<Integer> qtyMenuMinuman;
+    private float jarak;
+    private long total;
+}
