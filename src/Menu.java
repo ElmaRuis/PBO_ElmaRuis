d73f741c1c46184022210d13eeaef4ed709dee25
ElmaRuis <elmatampubolon@gmail.com>
2023-04-17 09:18:04 +0800

    Tampilan Menu Restaurant

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+    public class Menu {
+        private String nama;
+        private long harga;
+
+        public Menu(){
+            //Default constructor
+        }
+
+        public Menu(String nama, long harga) {
+            this.nama = nama;
+            this.harga = harga;
+        }
+
